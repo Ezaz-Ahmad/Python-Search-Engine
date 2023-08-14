db5e9ed94ff01f6bfb09adab672010afd9404d18
Ezaz Ahmad <ezazahmadshanto@gmail.com>
2023-08-15 01:46:13 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+try:
+    from googlesearch import search
+except ImportError:
+    print("No module named 'google' found. Please install it using 'pip install google'")
+
+
+
+
+def google_search(query):
+    try:
+        for j in search(query, num=10, stop=10, pause=2):
+            print(j)
+    except Exception as e:
+        print(e)
+while True:
+    user_input = input("Enter a number (0 to exit) or write your query: ")
+
+    if user_input == "0":
+        print("Exiting the program.")
+        break  # Exit the loop if user input is 0
+    if __name__ == "__main__":
+        query =user_input
+        google_search(query)
+    # You can add any other processing you want here
+    print(f"You entered: {user_input}")
+
